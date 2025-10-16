5eb85bd5ffaad4a57fd1eda602456ff5e712723a
yasuo3o3 <fujiwara@netservice.ne.jp>
2025-10-17 00:34:34 +0900

    追記

---
+++ build/blocks/notices-list/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n', 'wp-primitives'), 'version' => 'd234e6c8fa9e8c552e77');
